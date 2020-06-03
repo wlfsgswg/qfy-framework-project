88d637f31f90158ca22b4802ce70c438b1f131e2
王六锋 <2496436621@qq.com>
2020-06-03 12:37:22 +0800

    测试iframe table可行性

--- src/containers/Home/Personnel/index.jsx
+++ src/containers/Home/Personnel/index.jsx
@@ -1,13 +1,58 @@
 import React from "react";
 import { classPrefix } from "./../../../const";
 import "./index.less";
-import { MyIframe } from "./../../../components";
+// import { MyIframe } from "./../../../components";
 class Personnel extends React.Component {
+  componentDidMount() {
+    const iframeArr = document.querySelectorAll(".iframe-content");
+    if (iframeArr.length) {
+      let bol = false;
+      for (let i = 0; i < iframeArr.length; i++) {
+        bol = bol || iframeArr[i].getAttribute("title") === "智能人事";
+      }
+      if (!bol) {
+        const iframe = document.createElement("iframe");
+        iframe.setAttribute("src", "http://192.168.1.9:9024/main#/");
+        iframe.setAttribute("class", "iframe-content");
+        iframe.setAttribute("title", "智能人事");
+        iframe.setAttribute("style", "display:inline-block;");
+        document.getElementById("root").append(iframe);
+      } else {
+        const iframeArr = document.querySelectorAll(".iframe-content");
+        for (let i = 0; i < iframeArr.length; i++) {
+          if (iframeArr[i].getAttribute("title") === "智能人事") {
+            iframeArr[i].setAttribute("style", "display:inline-block;");
+          }
+        }
+      }
+    } else {
+      const iframe = document.createElement("iframe");
+      iframe.setAttribute("src", "http://192.168.1.9:9024/main#/");
+      iframe.setAttribute("class", "iframe-content");
+      iframe.setAttribute("title", "智能人事");
+      iframe.setAttribute("style", "display:inline-block;");
+      document.getElementById("root").append(iframe);
+    }
+  }
+
+  componentWillUnmount() {
+    const iframeArr = document.querySelectorAll(".iframe-content");
+    console.log(iframeArr);
+
+    for (let i = 0; i < iframeArr.length; i++) {
+      if (iframeArr[i].getAttribute("title") === "智能人事") {
+        // console.log(11111);
+        iframeArr[i].setAttribute("style", "display:none;");
+      }
+    }
+  }
+
   render() {
     return (
       <div className={`${classPrefix}-home-personnel`}>
         <div className={`${classPrefix}-home-personnel-content`}>
-          <MyIframe src="http://192.168.1.9:9024/main#/" title="智能人事" />
+          <div id="qfy-znrs"></div>
+          {/* <MyIframe src="http://192.168.1.9:9024/main#/" title="智能人事" /> */}
         </div>
       </div>
     );
