38c25e37ecd5c3f7405a3e8650b45b75c354a327
王六锋 <2496436621@qq.com>
2020-06-11 19:27:58 +0800

    no message

--- src/containers/Home/User/index.jsx
+++ src/containers/Home/User/index.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import { classPrefix } from "./../../../const";
+import { Title } from "./../../../components";
+import "./index.less";
+class User extends React.Component {
+  constructor(props) {
+    super(props);
+    this.setState = {
+      detail: {},
+    };
+  }
 
-class User extends React.Component{
+  componentDidMount() {
+    console.log(111);
+    this.handleGetUserDetail();
+  }
+
+  handleGetUserDetail = () => {
+    Request.post(`/aAcct/bInfo`).then((res) => {
+      console.log(res.data, res.data.data);
+    });
+  };
   render() {
-    return<div>User</div>
+    return (
+      <div className={`${classPrefix}-home-user`}>
+        <div className={`${classPrefix}-home-user-content`}>
+          <Title>个人信息</Title>
+          <div className={`${classPrefix}-home-user-content-top`}></div>
+        </div>
+      </div>
+    );
   }
 }
 
-export default User
+export default User;
